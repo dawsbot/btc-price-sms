15ac8af1e1441fed790ba3d4834874238ac84cd4
Dawson Botsford <3408480+dawsbot@users.noreply.github.com>
2015-06-21 22:27:20 -0700

    Fix port definition style

--- index.js
+++ index.js
@@ -37,8 +37,7 @@ app.post('/', function(request, response) {
     });
 });
 
-app.set('port', process.env.PORT || 3000);
 
-app.listen(app.get('port'), function() {
-       console.log('Express serving on port ' + app.get('port'));
+app.listen(process.env.PORT || 3000, function() {
+       console.log('Express serving');
 });
